de2affa905bb417cf295d6782b022520e7c1beb1
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2022-11-02 11:45:58 -0400

    Initial commit

---
+++ src/main/java/com/plannerssystem/plannerssystem/PlannerssystemApplication.java
@@ -0,0 +1,13 @@
+package com.plannerssystem.plannerssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerssystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plannerssystem/plannerssystem/PlannerssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plannerssystem.plannerssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerssystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
